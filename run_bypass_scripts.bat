be6dadaa436212a28d2cdfacd8743e02f92d984b
JamesPoppyCock <42442664+jamesly123@users.noreply.github.com>
2025-07-15 23:21:48 +0800

    Update run_bypass_scripts.bat

--- run_bypass_scripts.bat
+++ run_bypass_scripts.bat
@@ -1,49 +1,41 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: Prompt the user for the target app package name and the folder containing Frida scripts
+:: Prompt for target app and script folder
 set /p targetApp="Enter the target app package name (e.g., com.example.app): "
-set /p scriptFolder="Which platform is it (path to script folder): "
-set /p connectionType="Is this a remote device? (yes/y or no/n): "
+set /p scriptFolder="Enter the path to the folder containing Frida scripts: "
 set /p typeofScriptsUsed="Testing for SSL Pinning or Root/JB Detection? (S/s or D/d): "
 
-:: Normalize the input to lowercase for consistency
-for %%A in (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
-    set "connectionType=!connectionType:%%A=%%A!"
-)
-
-:: Set initial Frida connection option based on remote or USB
-if /I "%connectionType%" EQU "yes" (
-    set /p ipAddr="Enter the IP address of the target: "
-    set "fridaOption=-H %ipAddr%"
-) else if /I "%connectionType%" EQU "y" (
-    set /p ipAddr="Enter the IP address of the target: "
-    set "fridaOption=-H %ipAddr%"
-) else (
-    set "fridaOption=-U"
-)
-
-:: Ask if the user wants to specify a specific device ID (overrides -U)
-set /p specifyDevice="Do you want to specify a specific device ID? (yes/y or no/n): "
+:: Ask if the user wants to specify a device ID (overrides other connection options)
+set /p specifyDevice="Do you want to specify a specific Frida device ID? (yes/y or no/n): "
 if /I "%specifyDevice%" EQU "yes" (
     set /p deviceID="Enter the device ID (e.g., emulator-5556): "
     set "fridaOption=-D %deviceID%"
+) else (
+    :: Ask if the device is remote
+    set /p connectionType="Is this a remote device? (yes/y or no/n): "
+    if /I "%connectionType%" EQU "yes" (
+        set /p ipAddr="Enter the IP address of the remote device: "
+        set "fridaOption=-H %ipAddr%"
+    ) else if /I "%connectionType%" EQU "y" (
+        set /p ipAddr="Enter the IP address of the remote device: "
+        set "fridaOption=-H %ipAddr%"
+    ) else (
+        :: Default to USB
+        set "fridaOption=-U"
+    )
 )
 
-:: Check if the specified script folder exists
+:: Verify script folder exists
 if not exist "%scriptFolder%" (
     echo The specified folder path does not exist. Exiting.
     exit /b
 )
 
-:: Determine script subfolders to search
+:: Determine script subfolder based on type
 if /I "%typeofScriptsUsed%" EQU "s" (
     set "scriptFolder=%scriptFolder%\SSL"
-) else if /I "%typeofScriptsUsed%" EQU "S" (
-    set "scriptFolder=%scriptFolder%\SSL"
 ) else if /I "%typeofScriptsUsed%" EQU "d" (
-    set "scriptFolder=%scriptFolder%\Detection"
-) else if /I "%typeofScriptsUsed%" EQU "D" (
     set "scriptFolder=%scriptFolder%\Detection"
 ) else (
     echo Invalid script type entered. Exiting.
@@ -57,22 +49,21 @@ for %%f in ("%scriptFolder%\*.js") do (
     set /a scriptCount+=1
 )
 
-:: Ensure there is at least one Frida script
+:: Ensure at least one script was found
 if %scriptCount% EQU 0 (
-    echo No .js scripts found in the specified folder. Exiting.
+    echo No .js scripts found in %scriptFolder%. Exiting.
     exit /b
 )
 
-:: Run each Frida script on the target app
+:: Execute each Frida script using the selected connection option
 set /A end=%scriptCount%-1
 for /L %%i in (0,1,!end!) do (
-    set "scriptPath=.\!scriptFiles[%%i]!"
+    set "scriptPath=%%~f"
     set /A scriptCurr=%%i+1
-    echo Running !scriptCurr!/%scriptCount%
-    echo Running Frida script: !scriptPath! on app: %targetApp%
-    frida %fridaOption% -f "%targetApp%" -l "!scriptPath!"
+    echo Running script !scriptCurr! of %scriptCount%: !scriptFiles[%%i]!
+    frida %fridaOption% -f "%targetApp%" -l "!scriptFiles[%%i]!"
     pause
 )
 
-echo All scripts have been executed.
+echo All scripts executed.
 pause
